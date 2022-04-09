9e4f2f99ada7f0ae1b0b8211615b4966ee45f68e
Anindya Sadhukhan <anindyasadhukhan1999@gmail.com>
2022-04-09 20:35:50 +0530

    ME_BUILDOUT_XHARKTANK_MODULE_BASIC

--- server_run.sh
+++ server_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This script must contain a series of commands to run the backend application
 # Eg:- For Node.js project;
+  cd backend
   npm run start
 # Eg:- For SpringBoot Project;
 #       ./gradlew bootRun
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This script must contain a series of commands to setup dependencies of your project to make it work.
 # Eg:- For Node.js project;
+  cd backend
   npm install
 # Eg:- For Java Gradle Project;
 #       ./gradlew build
